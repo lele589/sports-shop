b4989351a6fad697a03b09f6092389d58d18dee9
elenacarrasco <elena.carrasco@zinklar.com>
2025-01-03 11:37:57 +0100

    test: add social test example

--- src/ui/components/ProductDetail.test.tsx
+++ src/ui/components/ProductDetail.test.tsx
@@ -1,7 +1,6 @@
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import ProductDetail from './ProductDetail';
 import { productRepository } from '../../infrastructure/repositories/http/productRepository';
-import { useCases } from '../../application/useCases/useCasesContainer';
 
 jest.mock('../../infrastructure/repositories/http/productRepository', () => ({
   productRepository: {
@@ -10,18 +9,18 @@ jest.mock('../../infrastructure/repositories/http/productRepository', () => ({
 }));
 
 const mockProductDefaults = {
-  id: 'anId',
+  id: 1,
   name: 'aTitle',
   description: 'aDescription',
   basePrice: 1000,
   stock: 5,
   parts: [
     {
-      id: '1',
+      id: 1,
       name: 'Part 1',
       options: [
-        { id: 'anOptionId', name: 'anOption', additionalPrice: 0, stock: 1 },
-        { id: 'anotherOptionId', name: 'anotherOption', additionalPrice: 10, stock: 3 },
+        { id: 1, name: 'anOption', additionalPrice: 0, stock: 1 },
+        { id: 2, name: 'anotherOption', additionalPrice: 10, stock: 3 },
       ],
     },
   ],
@@ -46,6 +45,7 @@ describe('ProductDetail', () => {
       expect(getByText(/aDescription/i)).toBeInTheDocument();
     });
   });
+
   it('should display loading when a product is not found', () => {
     (productRepository.findProductById as jest.Mock).mockImplementation(
       () => new Promise(() => {}),
@@ -56,4 +56,36 @@ describe('ProductDetail', () => {
     expect(getByText(/loading/i)).toBeInTheDocument();
   });
 
+  it('should update product price when an option is selected', async () => {
+    const mockProduct = {
+      ...mockProductDefaults,
+      basePrice: 500,
+      parts: [
+        {
+          id: 1,
+          name: 'Part 1',
+          options: [{ id: 1, name: 'Option 1', additionalPrice: 10, stock: 5 }],
+        },
+      ],
+      dependencies: [],
+    };
+
+    (productRepository.findProductById as jest.Mock).mockResolvedValue({
+      success: true,
+      data: mockProduct,
+    });
+
+    const container = render(<ProductDetail />);
+
+    await waitFor(() => {
+      expect(container.getByText(/500/i)).toBeInTheDocument();
+    });
+
+    const option = container.getByRole('button', { name: /Option 1/i });
+    fireEvent.click(option);
+
+    await waitFor(() => {
+      expect(container.getByText(/510/i)).toBeInTheDocument();
+    });
+  });
 });
